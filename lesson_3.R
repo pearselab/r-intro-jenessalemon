7bfff88b726fd563ddcc40e2e4e5da644fd23a64
Jenessa Lemon <jimblotter@Jenessa.local>
2016-10-26 20:59:16 -0600

    just another commit

--- lesson_3.R
+++ lesson_3.R
@@ -41,9 +41,10 @@ new.point <- function(x,y){
   class(output) <- "point"
   return(output)
 }
-point_1 <- new.point(x=1,y=2)
+point_1 <- new.point(x=1, y=2)
 point_2 <- new.point(x=2, y=2)
-point_1
+point_3 <- new.point(x=2, y=3)
+point_4 <- new.point(x=1, y=5)
 
 #3.
 #Function to create new instances of the class "point"
@@ -56,12 +57,38 @@ distance <- function(point1,point2) {
 distance(point_1, point_2)
 
 #4.
-"""new.line <- function(point1, point2){
+new.line <- function(point1, point2){
   output <- list(point1, point2)
   class(output) <- "line"
-  return(output)"""
+  return(output)
+}
+line_1 <- new.line(point_1, point_2)
+line_2 <- new.line(point_3, point_4)
+plot(point_1, point_2)
+#5.
+new.polygon <- function(line1, line2, line3, line4){
+  output <- list(line1, line2, line3, line4)
+  class(output) <- "polygon"
+  return(output)
+}
+
+#6.
+plot.point <- function(x,y){
+  plot(point1, point2)
+}
+plot.line <- function(line1, line2){
+  if(!inherits(line1, "line") | !inherits(line2, "line")){
+    stop("You haven't given me two lines!")
+  }
+  plot(line1, line2)
+}
+
+
+
+polygon.generator <- new.polygon(point1, point2, point3, point4)
+if(!inherits(point1, "point") | !inherits(point2, "point"), !inherits(point3, "point") | !inherits(point4, "point"))
+  stop("You haven't given me four points!")
+    out = list(new.line(point1, point2), new.line(point2, point3), new.line(point3, point4), new.line(point4, point1))
+      return(out)
+
   
-line <- function(point1, point2)
-  if(!inherits(point1, "point") | !inherits(point2, "point"))
-    stop("You haven't given me two points!")
-  plot()
