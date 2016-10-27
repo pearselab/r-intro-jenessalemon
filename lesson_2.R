68fd86774f542f127029406214ed4ce2f2aaa412
Your Name <jimblotter@Jenessa.local>
2016-10-27 10:06:17 -0600

    Not afraid of Commitment

---
+++ lesson_2.R
@@ -0,0 +1,57 @@
+value <- 5          #returns "Good news!"
+if(value <= 5){
+  print("Good news!")
+}
+
+value <- 5          #returns "Greater than or equal to five!"
+if(value < 5){
+  print("Less than five!")
+} else {
+  print("Greater than or equal to five!")
+}
+
+Exercises:
+#1
+  for (i in 20:10){
+  print (i)
+  }
+
+#2
+is.even <- function(x) {
+  (x %% 2 == 0) 
+} 
+for (i in 20:10) {
+    if (is.even(i)) {
+        print (i)
+    }
+} 
+
+#3"
+is.prime <- function(x){
+  for (i in 2:(x-1)) {
+    flag = x %% i
+    if (flag == 0) {
+      out <- c(x,"is not a prime number.")
+      #print(out)
+      return(FALSE)
+      }
+    }
+  out2 <- c(x,"is a prime number!")
+  #print(out2)
+  return(TRUE)
+}
+
+is.prime(7)
+is.prime(8)
+is.prime(10)
+
+#4
+for (i in 1:20){
+  if (i %% 5 == 0){
+    print("Good: NUMBER")
+    print(i)
+    if (i %% 5 == 0 & is.prime(i) == TRUE){
+      print("Job: NUMBER")
+      print(i)}
+  }
+}
